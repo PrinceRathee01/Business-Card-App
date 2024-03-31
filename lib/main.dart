5e3ab20d930b2a4d17d92ffb478d82b1899f45ca
PrinceRathee01 <pranaya107@gmail.com>
2024-03-31 15:11:14 +0530

    finalized background color

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
